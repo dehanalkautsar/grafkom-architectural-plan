0fc7d81234a565f216c1a508c25176b702b7cd41
dehanalkautsar <dehanalkautsar3@gmail.com>
2022-02-10 08:06:58 +0700

    feat: sekarang udh bisa nampilin bentuk scr manual di canvas

--- scripts/script.js
+++ scripts/script.js
@@ -44,16 +44,129 @@ function main() {
   // look up where the vertex data needs to go.
   var positionLocation = gl.getAttribLocation(program, "a_position");
   // look up uniform locations
-  var colorLocation = gl.getUniformLocation(program, "vertColor");
+  var colorLocation = gl.getAttribLocation(program, "vertColor"); //it should be uniform but, hey somehow it is work?!?! x_x
 
   // Create a buffer to put positions in
   var positionBuffer = gl.createBuffer();
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   // put geometry data into buffer
-  // setGeometry(gl);
+  setGeometry(gl);
 
-  // drawScene();
+  // create a buffer to put colors in
+  var colorBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  // set the colors
+  setColors(gl);
+
+  drawScene();
+
+  function drawScene() {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+    // tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    // Clear the canvas
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+    // Turn on the vertex attributes
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = gl.FALSE; // dont normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+
+    gl.vertexAttribPointer(
+      positionLocation,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+
+    // Turn on the colors attribute
+    gl.enableVertexAttribArray(colorLocation);
+    // Bind the color buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // Tell the color attribute how to get data out of colorBuffer (ARRAY_BUFFER)
+    var size = 4; // 4 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = gl.FALSE; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+      colorLocation,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+
+    // draw the geometry
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+  }
+}
+
+function setGeometry(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      -0.15, -0.1, 0.15, -0.1, -0.15, 0.1, 0.15, -0.1, -0.15, 0.1, 0.15, 0.1,
+    ]),
+    gl.STATIC_DRAW
+  );
+}
+
+function setColors(gl) {
+  // Pick 2 random colors.
+  var r1 = Math.random();
+  var b1 = Math.random();
+  var g1 = Math.random();
+  var r2 = Math.random();
+  var b2 = Math.random();
+  var g2 = Math.random();
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      r1,
+      b1,
+      g1,
+      1,
+      r1,
+      b1,
+      g1,
+      1,
+      r1,
+      b1,
+      g1,
+      1,
+      r2,
+      b2,
+      g2,
+      1,
+      r2,
+      b2,
+      g2,
+      1,
+      r2,
+      b2,
+      g2,
+      1,
+    ]),
+    gl.STATIC_DRAW
+  );
 }
 
 main();
